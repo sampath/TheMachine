edb0ae753c5ce3233e1c6ea2bc05a67726bd5e0e
Kenta Asai <kasai@ucsd.edu>
2018-05-09 21:38:44 -0700

    changes to add firebase to project

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,13 @@ var firebase = require("firebase");
 var bodyParser = require('body-parser');
 
 /* connect to firebase project here */
+var config = {
+  apiKey: "AIzaSyAa7xJzMvvYSZbBbfaTb3-k4eSBqzAYYsI",
+  authDomain: "flick-b0e2c.firebaseapp.com",
+  databaseURL: "https://flick-b0e2c.firebaseio.com",
+  storageBucket: "flick-b0e2c.appspot.com",
+};
+firebase.initializeApp(config);
 
 var app = express();
 
