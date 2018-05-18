31788c7a2786c70d9f5a9bafd8e9b736f21bee63
gvsaditya <gvsaditya@gmail.com>
2018-05-17 20:11:43 -0700

    Fixed Router Bug

--- src/Routes.js
+++ src/Routes.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/404";
+import Login from "./containers/Login";
 
 export default () =>
   <Switch>
     <Route path="/" exact component={Home} />
+    <Route path="/login" exact component={Login} />
     <Route component={NotFound} />
   </Switch>;
 
